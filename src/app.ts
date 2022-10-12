e676d366b2d96d01dfd9d46fe12da467c5652648
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2022-10-12 12:24:30 +0100

    setup project server

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import config from 'config'
 
+const port = config.get<number>('port')
 const app = express();
 
-app.listen(1337, ()=>{
-    console.log('running');
+app.listen(port, ()=>{
+    console.log('App is running');
     
 })
