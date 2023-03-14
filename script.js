4628a36f2deb98fe879a123a49ed75546a22a334
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-14 18:33:07 +0100

    [feat]:data and request function

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
