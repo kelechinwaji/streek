10055455f3d42bd5c76b277637d25b71a47c568c
kelechi Nwaji <78841792+kelechinwaji@users.noreply.github.com>
2023-03-14 18:38:21 +0100

    [feat]:template data function

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 const queryParamsContainer = document.querySelector('[data-query-params]')
 const requestHeadersContainer = document.querySelector('[data-request-headers]')
+const keyValueTemplate = document.querySelector("[data-value-template]")
 
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
+
+function createKeyValuePair(){
+    const element = keyValueTemplate.textContent.cloneNode(true)
+    element.querySelector('[data-remove-btn]').addEventListener('click', (e) =>{
+        e.target.closet('[data-key-value-pair]')
+    })
+}
